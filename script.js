9537449571efbf5d05fbe220dd9808810bbcdc14
Farman71 <farmanhussain71@gmail.com>
2025-02-26 13:09:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+document.addEventListener("DOMContentLoaded",function(){
+	let draggedElement=null;
+
+	let images=document.querySelectorAll(".image");
+	images.forEach(image=>{
+		image.addEventListener("dragstart",function(e)
+		{
+			draggedElement=this;
+		})
+
+		image.addEventListener("dragover",function(e){
+			e.preventDefault();
+		})
+		image.addEventListener("drop",function(e){
+			e.preventDefault(); 
+			if (draggedElement!==this) {
+				let temp= this.innerHTML
+				this.innerHTML= draggedElement.innerHTML
+				draggedElement.innerHTML=temp
+			}
+		})
+		image.addEventListener("dragend", function () {
+            this.classList.remove("selected"); // Remove highlight
+        });
+	})
+})
